5a33d0e7303dda6f370e91cc662ba96cef771103
Vladimir Rujević <vladimir.rujevic@live.com>
2025-03-10 19:13:39 +0100

    Count the characters number of occurences in the book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3
-from stats import count_words
+from stats import count_words, count_characters
 
 def get_book_text (path):
     with open(path) as f:
@@ -8,6 +8,8 @@ def get_book_text (path):
 def main ():
     book_text = get_book_text("books/frankenstein.txt")
     num_words = count_words(book_text)
+    character_count = count_characters(book_text)
     print(f"{num_words} words found in the document")
+    print(character_count)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,11 @@
 def count_words (text):
     words = text.split()
     return len(words)
+
+def count_characters (text):
+    result = {}
+    for c in text:
+        if c.lower() not in result:
+            result[c.lower()] = 0
+        result[c.lower()] += 1
+    return result
