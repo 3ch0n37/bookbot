6fa2fb184ee3eda496dd9870bbae2deefeab130c
Vladimir Rujević <vladimir.rujevic@live.com>
2025-03-10 19:33:34 +0100

    Generate a report

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 #! /usr/bin/env python3
-from stats import count_words, count_characters
+from stats import count_words, count_characters, generate_report
 
 def get_book_text (path):
     with open(path) as f:
         return f.read()
     
 def main ():
-    book_text = get_book_text("books/frankenstein.txt")
+    book_path = "books/frankenstein.txt"
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    book_text = get_book_text(book_path)
     num_words = count_words(book_text)
     character_count = count_characters(book_text)
-    print(f"{num_words} words found in the document")
-    print(character_count)
+    report = generate_report(character_count)
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for line in report:
+        print(f"{line["character"]}: {line["count"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -9,3 +9,17 @@ def count_characters (text):
             result[c.lower()] = 0
         result[c.lower()] += 1
     return result
+
+def sort_on_count(dict):
+    return dict["count"]
+
+def generate_report (character_count):
+    report = []
+    for k in character_count.keys():
+        if k.isalpha():
+            report.append({
+                "character": k,
+                "count": character_count[k]
+            })
+    report.sort(reverse=True, key=sort_on_count)
+    return report
